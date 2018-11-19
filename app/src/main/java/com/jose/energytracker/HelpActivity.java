e9d4f400a87e2dce8ae02dc5d826bc0e0072e9cc
JoseGIC <jm.perezo@alumnos.urjc.es>
2018-11-19 18:19:49 +0100

    Envio de mensaje de soporte hecho

--- app/src/main/java/com/jose/energytracker/HelpActivity.java
+++ app/src/main/java/com/jose/energytracker/HelpActivity.java
@@ -1,14 +1,24 @@
 package com.jose.energytracker;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button emailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        emailButton = (Button) findViewById(R.id.email_button);
+        emailButton.setOnClickListener(this);
     }
 
 
@@ -16,4 +26,34 @@ public class HelpActivity extends AppCompatActivity {
     public void onBackPressed() {
         finish();
     }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.email_button) {
+            sendEmail();
+        }
+    }
+
+
+    protected void sendEmail() {
+        String[] TO = {"energeat.support@gmail.com"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Escribe aquí tu mensaje");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
